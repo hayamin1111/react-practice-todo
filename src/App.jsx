f225b2c651c56768639786494046f50647fce081
hayakawa <52098983+hayamin1111@users.noreply.github.com>
2023-08-03 23:24:33 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるToDOは5個までです。</p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 
+const style = {
+  background: "#ebebeb",
+  width: "400px",
+  marginBottom: "10px",
+  padding: "10px",
+};
+
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
+        disabled={disabled}
         placeholder="ToDoを入力"
         value={todoText}
         // inputで変更があったらstateも変更するにはonChangeが必要
         onChange={onChange}
       ></input>
-      <button onClick={onClick}>追加</button>
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
